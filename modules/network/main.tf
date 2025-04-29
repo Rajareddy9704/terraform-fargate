ebe69f09051b0ef39c36f7596f6cd6e31854a41b
Rajareddy9704 <bhumireddyrajareddy01@gmail.com>
2025-04-29 18:24:28 +0530

    updated new files

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,25 +1,47 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = var.tags
+  tags                 = var.tags
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = var.tags
 }
 
 resource "aws_subnet" "public_subnet" {
-  count = length(var.public_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block = element(var.public_subnets, count.index)
-  availability_zone = element(var.availability_zones, count.index)
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
-  tags = var.tags
+  tags                    = var.tags
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags   = var.tags
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_subnet" "private_subnet" {
-  count = length(var.private_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block = element(var.private_subnets, count.index)
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets, count.index)
   availability_zone = element(var.availability_zones, count.index)
-  tags = var.tags
+  tags              = var.tags
 }
 
 
